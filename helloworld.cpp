397c94676a2852dab7c75596269eed69d6247db7
wxxsfxyzm <wxxsfxyzm@126.com>
2024-05-02 13:13:35 +0800

    switch to Linux work profile

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include "PerformanceMonitor.h"
+
 int main(int argc, char const* argv[])
 {
-    PerformanceMonitor pm;
+    //PerformanceMonitor pm;
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
